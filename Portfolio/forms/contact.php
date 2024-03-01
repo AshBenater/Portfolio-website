9f33817be6f8d8005b6ca50e602384f1100f4ce3
AshBenater <131183117+AshBenater@users.noreply.github.com>
2024-03-01 19:22:13 +0000

    Completed Updated Portfolio

--- Portfolio/forms/contact.php
+++ Portfolio/forms/contact.php
@@ -10,37 +10,26 @@ require '../assets/vendor/PHPMailer/PHPMailer.php';
 require '../assets/vendor/PHPMailer/SMTP.php';
 
 $mail = new PHPMailer(true);
-$message = ''; // Initialize the message variable
+$message = ''; 
 
-try {
-    // Server settings
-    // $mail->SMTPDebug = 2;                   // Enable verbose debug output
+try {                
     $mail->isSMTP();
     $mail->Host = 'smtp.hostinger.com';
     $mail->SMTPAuth = true;
     $mail->Username = EMAIL_USER;
-    $mail->Password = EMAIL_PASS;  // Ensure this is correctly sourced from your config file
-    $mail->SMTPSecure = 'ssl';  // or 'tls' if using port 587
-    $mail->Port = 465;  // or 587 for TLS
-
-    // Recipients
+    $mail->Password = EMAIL_PASS;  
+    $mail->SMTPSecure = 'ssl'; 
+    $mail->Port = 465;  
     $mail->setFrom('ash@ashleighbenater.com', 'Ashleigh Benater');
-    $mail->addAddress('ash@ashleighbenater.com', 'Ashleigh Benater'); // Add a recipient
-
-    // Content
-    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->addAddress('ash@ashleighbenater.com', 'Ashleigh Benater'); 
+    $mail->isHTML(true);                                 
     $mail->Subject = $_POST['subject'];
     $mail->Body = 'Name: ' . $_POST['name'] . '<br>Email: ' . $_POST['email'] . '<br>Message: ' . $_POST['message'];
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
     $mail->send();
-
-    // Assign a success message to the $message variable
     echo 'OK';
 } catch (Exception $e) {
     $message = 'Message could not be sent. Please try again later.';
 }
-
-// Display the message (either success or error)
 echo $message;
 ?>
